22d362fe17c8137ad26dd08837b60c2b94d90dd0
Chris Thompson <eagleapex@gmail.com>
2011-07-12 10:43:05 -0400

    accel tweka

--- Tonokip_Firmware/configuration.h
+++ Tonokip_Firmware/configuration.h
@@ -29,10 +29,10 @@ long max_acceleration_units_per_sq_second = 2000; // Max acceleration in mm/s^2 
 long max_travel_acceleration_units_per_sq_second = 2000; // Max acceleration in mm/s^2 for travel moves
 #endif
 #ifdef EXP_ACCELERATION
-float full_velocity_units = 5; // the units between minimum and G1 move feedrate
-float travel_move_full_velocity_units = 5; // used for travel moves
-float min_units_per_second = 35; // the minimum feedrate
-float min_constant_speed_units = .2; // the minimum units of an accelerated move that must be done at constant speed
+float full_velocity_units = 6; // the units between minimum and G1 move feedrate
+float travel_move_full_velocity_units = 6; // used for travel moves
+float min_units_per_second = 20; // the minimum feedrate
+float min_constant_speed_units = .1; // the minimum units of an accelerated move that must be done at constant speed
                                     // Note that if the move is shorter than this value, acceleration won't be perfomed,
                                     // but will be done at the minimum between min_units_per_seconds and move feedrate speeds.
 #endif
